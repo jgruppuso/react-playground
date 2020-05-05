bd5785f713c543d3c4b7f05c7d51669be2ce50fe
Jay Gruppuso <j.gruppuso@gmail.com>
2020-05-05 23:39:50 +0900

    Updates

---
+++ precache-manifest.e4ddaf5ffea943c1a551f6629aa77267.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4da24a94f033503ffdc07279a22e524",
+    "url": "/react-playground/index.html"
+  },
+  {
+    "revision": "84b2b565f9d42646392f",
+    "url": "/react-playground/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "af9732de597d98fd6b38",
+    "url": "/react-playground/static/js/2.c8504ec5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-playground/static/js/2.c8504ec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b2b565f9d42646392f",
+    "url": "/react-playground/static/js/main.57469565.chunk.js"
+  },
+  {
+    "revision": "18197a02e882dcfa1097",
+    "url": "/react-playground/static/js/runtime-main.e989b3d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-playground/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de5f86d6d14a30b84b9d26b32fcb7689.js"
+  "/react-playground/precache-manifest.e4ddaf5ffea943c1a551f6629aa77267.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-playground/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
